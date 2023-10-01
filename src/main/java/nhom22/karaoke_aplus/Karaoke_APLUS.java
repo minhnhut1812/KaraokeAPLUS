3e08043a4e1667f2719f082d11cfe8a7980557b3
hominhhau <hauxoan333@gmail.comhau>
2023-10-01 23:51:44 +0700

    create Maven project and config pom.xml

---
+++ src/main/java/nhom22/karaoke_aplus/Karaoke_APLUS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package nhom22.karaoke_aplus;
+
+/**
+ *
+ * @author HO MINH HAU
+ */
+public class Karaoke_APLUS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!Hello Hồ Minh Hậu");
+    }
+}
