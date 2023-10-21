56b82b7a650230806cee86042dcdb36bcff567a1
hominhhau <hauxoan333@gmail.comhau>
2023-10-22 02:04:08 +0700

    add icon menu and title karaokeAplus

--- src/main/java/gui/component/Menu.java
+++ src/main/java/gui/component/Menu.java
@@ -64,7 +64,7 @@ public class Menu extends javax.swing.JPanel {
             panelSetting.add(new MenuItem(menu, getEventMenu(), event, panelItem.getComponentCount()), "h 30!, pushy, growy");
             
         } else {
-            panelItem.add(new MenuItem(menu, getEventMenu(), event, panelItem.getComponentCount()), "h 52!");
+            panelItem.add(new MenuItem(menu, getEventMenu(), event, panelItem.getComponentCount()), "h 53!");
         }
     }
 
@@ -96,8 +96,9 @@ public class Menu extends javax.swing.JPanel {
         Menu = new javax.swing.JPanel();
         panelItem = new javax.swing.JPanel();
         panelSetting = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
+        panelPopUpMenu = new javax.swing.JPanel();
         button1 = new gui.swing.Button();
+        jLabel1 = new javax.swing.JLabel();
 
         Menu.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -123,11 +124,11 @@ public class Menu extends javax.swing.JPanel {
         panelItem.setLayout(panelItemLayout);
         panelItemLayout.setHorizontalGroup(
             panelItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 190, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         panelItemLayout.setVerticalGroup(
             panelItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 517, Short.MAX_VALUE)
+            .addGap(0, 519, Short.MAX_VALUE)
         );
 
         panelSetting.setBackground(new java.awt.Color(255, 255, 255));
@@ -145,25 +146,37 @@ public class Menu extends javax.swing.JPanel {
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        jPanel1.setOpaque(false);
+        panelPopUpMenu.setOpaque(false);
 
-        button1.setBackground(new java.awt.Color(0, 166, 180));
-        button1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/open-menu.png"))); // NOI18N
+        button1.setBackground(new java.awt.Color(0, 136, 169));
+        button1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/Menu_1.png"))); // NOI18N
+        button1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                button1ActionPerformed(evt);
+            }
+        });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 3, 17)); // NOI18N
+        jLabel1.setText("KARAOKE APLUS");
+
+        javax.swing.GroupLayout panelPopUpMenuLayout = new javax.swing.GroupLayout(panelPopUpMenu);
+        panelPopUpMenu.setLayout(panelPopUpMenuLayout);
+        panelPopUpMenuLayout.setHorizontalGroup(
+            panelPopUpMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelPopUpMenuLayout.createSequentialGroup()
+                .addGap(16, 16, 16)
                 .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        panelPopUpMenuLayout.setVerticalGroup(
+            panelPopUpMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelPopUpMenuLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, Short.MAX_VALUE)
+                .addGroup(panelPopUpMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(button1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -171,20 +184,24 @@ public class Menu extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelItem, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(panelSetting, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelItem, javax.swing.GroupLayout.DEFAULT_SIZE, 215, Short.MAX_VALUE)
+            .addComponent(panelSetting, javax.swing.GroupLayout.DEFAULT_SIZE, 215, Short.MAX_VALUE)
+            .addComponent(panelPopUpMenu, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(panelPopUpMenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panelItem, javax.swing.GroupLayout.DEFAULT_SIZE, 517, Short.MAX_VALUE)
+                .addComponent(panelItem, javax.swing.GroupLayout.DEFAULT_SIZE, 519, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(panelSetting, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void button1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_button1ActionPerformed
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -203,8 +220,9 @@ public class Menu extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Menu;
     private gui.swing.Button button1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel panelItem;
+    private javax.swing.JPanel panelPopUpMenu;
     private javax.swing.JPanel panelSetting;
     // End of variables declaration//GEN-END:variables
 }
