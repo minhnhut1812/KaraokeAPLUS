c1a81f8a231ecf4be15898575438f43b9a6276d4
Quỳnh Giang <nguyenquynhgiang1324@gmail.com>
2023-10-22 18:19:08 +0700

    Login by QuynhGiang

---
+++ src/main/java/gui/Login.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+/**
+ *
+ * @author 84934
+ */
+public class Login {
+    public static void main(String[] args) {
+       
+    }
+}
